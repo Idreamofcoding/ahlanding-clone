25e8350e9bbbb98e9667ad6380f5b4128bf8dda3
Albert Rocha <rocharocharocha95@gmail.com>
2022-05-17 11:31:55 -0400

    made slider but still needs work

---
+++ slider_data.js
@@ -0,0 +1,22 @@
+const people = [
+    {
+      img:
+        "./images_ahlanding/pushup.jpg",
+      name: "Pushup Prestige",
+      price: "$40",
+    },
+    {
+      img:
+        "./images_ahlanding/shoulderpress_onehand.jpg",
+      name: "Boulder Shoulder",
+      price: "$50",
+    },
+    {
+      img:
+        "./images_ahlanding/slam_ball.jpg",
+      name: "Sweat by Slam",
+      price: "$80",
+    },
+  ];
+  
+  export default people;
